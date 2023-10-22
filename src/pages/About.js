820d807799ee6381745ab3e550b8d7d9cc19250c
Luis Manzo <lmanzo@uabc.edu.mx>
2023-10-21 20:42:25 -0700

    ajustes finales en aboutme

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className='flex flex-col lg:flex-row justify-center gap-y-8 lg:items-center lg:gap-x-12 '>
 
                 <div className='w-full flex justify-center ' >
-                    <img className='w-[200px] lg:w-[1200px] ' src={image1}   />
+                    <img className='w-[200px] lg:w-[1200px] ' src={image1} />
                 </div>
 
                 <div className='text-center lg:text-left lg:ml-9 text-white mx-10 lg:mx-2'>
@@ -20,18 +20,36 @@ const About = () => {
                     </div>
 
                     <p className='my-4'>
-                    I discovered the world of programming since high school, I always wondered how things work, I have asked myself that question since I was a child and with programming I have been able to realize many things, how the future is getting closer to us. as we have imagined and all thanks to technology that does not stop.               
+                        I entered the world of programming during my high school years, driven by a persistent curiosity about how things work. This curiosity has been with me since childhood, and through programming, I've been able to turn my wonder into tangible accomplishments. It's incredible to witness how the future is rapidly approaching in ways we once only imagined, and this transformation is made possible by the relentless progression of technology.
                     </p>
 
-       
-
-
                 </div>
-         
+
             </div>
 
 
-        
+
+            {/* <h1 class="text-2xl text-center mb-8">Mi Trayectoria Profesional</h1>
+
+            <div class="timeline">
+            <div class="timeline-item">
+                    <p>BAJANET</p>
+                    <p>SEDENA APP</p>
+                    <p>2020</p>
+                </div>
+                <div class="timeline-item">
+                    <p>Maypa</p>
+                    <p>Payment gateway</p>
+                    <p>2021</p>
+                </div>
+                <div class="timeline-item">
+                    <p>Mino Logistics</p>
+                    <p>Web App, Server App, App</p>
+                    <p>2021-2023</p>
+                </div>
+            </div> */}
+
+
         </dic>
 
     </ div>
