912ab1f65cb6b4b82d8df24e9705d8d5d04eff95
Luis Manzo <lmanzo@uabc.edu.mx>
2023-10-21 20:50:10 -0700

    ajustes finales en banner

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -9,16 +9,16 @@ const Banner = () => {
 
             <div className='flex flex-col-reverse lg:flex-row justify-center lg:items-center  mx-10 lg:mx-2'>
 
-                <div className='text-center lg:text-left lg:ml-9 text-white'>
+                <div className='text-center lg:text-left lg:ml-9 text-white px-2 lg:w-[50%] '>
                     <h2 className="mb-2 text-3xl lg:text-5xl font-thin ">
                         Hello!
                     </h2>
-                    <h1 className='mb-8 text-3xl lg:text-7xl font-semibold gradient-text h-[78px]'>
+                    <h1 className='mb-1 text-3xl lg:text-7xl font-semibold gradient-text h-[78px]'>
                         I'm Luis Angel
                     </h1>
 
-                    <p><strong >Backend</strong> and  <strong >Frontend</strong> developer with more than 6 years of experience,</p>
-                    <p>passionate about learning new technologies to continue growing.</p>
+                    <p className='lg:text-2xl  '><strong >Backend</strong> and  <strong >Frontend</strong> developer with more than 6 years of experience, passionate about learning new technologies to continue growing.</p>
+
 
                 </div>
                 <div className=' flex justify-center items-center '>
